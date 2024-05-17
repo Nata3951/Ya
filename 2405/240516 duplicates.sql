e83703506086ea65bcda51244b8ec10c3d02f707
Nata3951 <Nata3951@gmail.com>
2024-05-17 11:23:40 +0300

    Update 240516 duplicates.sql

--- 2405/240516 duplicates.sql
+++ 2405/240516 duplicates.sql
@@ -6,3 +6,17 @@ select distinct `subscription_id`, `profession_name`, `user_id`
 from $t
 where user_id IN (4785451, 3037)
 ;
+
+
+-- checking that users do not duplicate in user list
+
+PRAGMA AnsiInForEmptyOrNullableItemsCollections;
+
+use hahn;
+
+$t = 'home/praktikum/prod/datalake/db_transfer_personal/accounts_user';
+
+select id, `yauid`,`username`
+from $t
+where id IN (4785451, 3037)
+;
