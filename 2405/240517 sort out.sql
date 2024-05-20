17549bed46f30f6b60de97ee05181951491741af
Nata3951 <Nata3951@gmail.com>
2024-05-20 18:15:42 +0300

    Update 240517 sort out.sql

--- 2405/240517 sort out.sql
+++ 2405/240517 sort out.sql
@@ -7,3 +7,21 @@ SELECT
 FROM my_table
 WINDOW w AS (ORDER BY key);
 Дока: https://yql.yandex-team.ru/docs/yt/builtins/window?searchQuery=IGNORE%20NULLS#first_value-/-last_value 
+
+
+$some_date = DATE('2022-01-01')
+
+-- 
+FROM AS_TABLE(
+    AsList(
+        AsStruct(1 AS Key, AsList(1, 2, 3, 4) AS Value),
+        AsStruct(4 AS Key, AsList(4, 5, 6, 7) AS Value),
+        AsStruct(2 AS Key, AsList(7, 8, 9, 0) AS Value)
+    )
+)
+
+--
+DECLARE $x AS String;
+DECLARE $y AS String?;
+DECLARE $z AS List<String>;
+SELECT $x, $y, $z;
