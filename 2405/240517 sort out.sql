84afe8e1a9448ee2536868328ca1b907fe58d3c1
Nata3951 <Nata3951@gmail.com>
2024-05-20 16:20:20 +0300

    Update 240517 sort out.sql

--- 2405/240517 sort out.sql
+++ 2405/240517 sort out.sql
@@ -1,1 +1,9 @@
 select region, Geo::RegionById(cast(region as Int32)).name as region_name,
+
+
+-- получить в колонке первое непустое значение
+SELECT
+   FIRST_VALUE(my_column) IGNORE NULLS OVER w
+FROM my_table
+WINDOW w AS (ORDER BY key);
+Дока: https://yql.yandex-team.ru/docs/yt/builtins/window?searchQuery=IGNORE%20NULLS#first_value-/-last_value 
