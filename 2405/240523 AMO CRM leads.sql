f4e87df95250bef2d57b4080ceeb5798c532a72c
Nata3951 <Nata3951@gmail.com>
2024-05-23 15:20:27 +0300

    Update 240523 AMO CRM leads.sql

--- 2405/240523 AMO CRM leads.sql
+++ 2405/240523 AMO CRM leads.sql
@@ -1,1 +1,10 @@
+-- days to payment
 
+$leads = "home/praktikum/prod/analytics/dashboards/amocrm_leads";
+
+IF(
+  [is_paid] 
+  AND NOT [is_robot] 
+  AND [payment_ts] > [created_ts], 
+  ([payment_ts] - [created_ts]), 
+  NULL)
